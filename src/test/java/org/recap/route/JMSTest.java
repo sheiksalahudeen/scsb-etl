eb8c26aee74607d2218d56a4ca20c17dd6a2b831
Peri Health <info@perihealth.com>
2016-06-28 19:57:18 -0400

    Updated JMS Test.

--- src/test/java/org/recap/route/JMSTest.java
+++ src/test/java/org/recap/route/JMSTest.java
@@ -13,9 +13,12 @@ import org.apache.camel.util.jndi.JndiContext;
 import org.junit.Test;
 import org.recap.BaseTestCase;
 import org.recap.model.jpa.BibliographicEntity;
+import org.recap.model.jpa.HoldingsEntity;
+import org.recap.model.jpa.ItemEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Random;
 
 import static junit.framework.TestCase.assertEquals;
@@ -60,12 +63,38 @@ public class JMSTest extends BaseTestCase {
             }
         });
 
+
+
         Random random = new Random();
         for(int i = 0; i < 10; i++){
             BibliographicEntity bibliographicEntity1 = new BibliographicEntity();
             bibliographicEntity1.setBibliographicId(random.nextInt());
+            bibliographicEntity1.setOwningInstitutionBibId(String.valueOf(random.nextInt()));
+            bibliographicEntity1.setOwningInstitutionId(1);
+            bibliographicEntity1.setContent("mock content");
+            bibliographicEntity1.setCreatedDate(new Date());
+
+            HoldingsEntity holdingsEntity = new HoldingsEntity();
+            holdingsEntity.setContent("mock holding content");
+            holdingsEntity.setCreatedDate(new Date());
+            ItemEntity itemEntity = new ItemEntity();
+            itemEntity.setBarcode("b.123");
+            itemEntity.setCollectionGroupId(1);
+            itemEntity.setCreatedDate(new Date());
+            itemEntity.setOwningInstitutionId(1);
+            itemEntity.setCustomerCode("PA");
+            itemEntity.setOwningInstitutionItemId("123");
+            holdingsEntity.setItemEntities(Arrays.asList(itemEntity));
+            bibliographicEntity1.setHoldingsEntities(Arrays.asList(holdingsEntity));
+
             BibliographicEntity bibliographicEntity2 = new BibliographicEntity();
             bibliographicEntity2.setBibliographicId(random.nextInt());
+            bibliographicEntity2.setOwningInstitutionBibId(String.valueOf(random.nextInt()));
+            bibliographicEntity2.setContent("mock conent");
+            bibliographicEntity2.setCreatedDate(new Date());
+            bibliographicEntity2.setOwningInstitutionId(1);
+            bibliographicEntity1.setHoldingsEntities(Arrays.asList(holdingsEntity));
+
             producer.sendBody("activemq:queue:testQ", Arrays.asList(bibliographicEntity1, bibliographicEntity2));
         }
 
