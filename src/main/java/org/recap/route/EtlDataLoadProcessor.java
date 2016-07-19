27a95969d2f4678c85f49c72944ff4a72883b72f
peris <peri@periinc.com>
2016-07-19 16:15:01 -0400

    Updated ETLDataLoadProcessor

--- src/main/java/org/recap/route/EtlDataLoadProcessor.java
+++ src/main/java/org/recap/route/EtlDataLoadProcessor.java
@@ -50,13 +50,13 @@ public class EtlDataLoadProcessor {
                 logger.info("Time taken to save: " + xmlRecordEntities.getNumberOfElements() + " bib and related data is: " + (endTime - startTime) / 1000 + " seconds.");
             }
 
-            recordProcessor.shutdownExecutorService();
 
             long totalEndTime = System.currentTimeMillis();
             logger.info("Time taken to save: " + xmlRecordEntities.getTotalElements() + " bib and related data is: " + (totalEndTime - totalStartTime) / 1000 + " seconds.");
         } else {
             logger.info("No records found to load into DB");
         }
+        recordProcessor.shutdownExecutorService();
     }
 
     public Integer getBatchSize() {
