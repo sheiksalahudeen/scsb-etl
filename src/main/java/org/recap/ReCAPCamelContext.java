45ca79d60c8ddd906eb5aa87dbd85907fb5fe24a
angelindayana <angelin.dayana@gmail.com>
2016-07-21 21:55:38 +0530

    Added the RouteBuilder for uploading files to FTP.

--- src/main/java/org/recap/ReCAPCamelContext.java
+++ src/main/java/org/recap/ReCAPCamelContext.java
@@ -4,6 +4,7 @@ import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.recap.repository.XmlRecordRepository;
+import org.recap.route.FTPRouteBuilder;
 import org.recap.route.JMSReportRouteBuilder;
 import org.recap.route.XmlRouteBuilder;
 import org.slf4j.Logger;
@@ -27,22 +28,36 @@ public class ReCAPCamelContext {
     private String inputDirectoryPath;
     private Integer poolSize;
     private Integer maxPoolSize;
+    private String reportsDirectory;
+    private String ftpRemoteServer;
+    private String ftpUserName;
+    private String ftpKnownHost;
+    private String ftpPrivateKey;
 
     private JMSReportRouteBuilder jmsReportRouteBuilder;
     private XmlRouteBuilder xmlRouteBuilder;
+    private FTPRouteBuilder ftpRouteBuilder;
 
 
     @Autowired
     public ReCAPCamelContext(CamelContext context, XmlRecordRepository xmlRecordRepository,
                              @Value("${etl.split.xml.tag.name}") String xmlTagName,
                              @Value("${etl.load.directory}") String inputDirectoryPath,
-                             @Value("${etl.pool.size}") Integer poolSize, @Value("${etl.max.pool.size}") Integer maxPoolSize) {
+                             @Value("${etl.pool.size}") Integer poolSize, @Value("${etl.max.pool.size}") Integer maxPoolSize,
+                             @Value("${etl.report.directory}") String reportsDirectory, @Value("${ftp.userName}") String ftpUserName,
+                             @Value("${ftp.remote.server}") String ftpRemoteServer, @Value("${ftp.knownHost}") String ftpKnownHost,
+                             @Value("${ftp.privateKey}") String ftpPrivateKey) {
         this.context = context;
         this.xmlRecordRepository = xmlRecordRepository;
         this.xmlTagName = xmlTagName;
         this.inputDirectoryPath = inputDirectoryPath;
         this.poolSize = poolSize;
         this.maxPoolSize = maxPoolSize;
+        this.reportsDirectory = reportsDirectory;
+        this.ftpRemoteServer = ftpRemoteServer;
+        this.ftpUserName = ftpUserName;
+        this.ftpKnownHost = ftpKnownHost;
+        this.ftpPrivateKey = ftpPrivateKey;
         init();
     }
 
@@ -62,6 +77,7 @@ public class ReCAPCamelContext {
     public void addDefaultRoutes() throws Exception {
         addRoutes(getXmlRouteBuilder());
         addRoutes(getJMSReportBuilder());
+        addRoutes(getFtpRouteBuilder());
     }
 
     private JMSReportRouteBuilder getJMSReportBuilder() {
@@ -85,5 +101,17 @@ public class ReCAPCamelContext {
             xmlRouteBuilder.setXmlRecordRepository(xmlRecordRepository);
         }
         return xmlRouteBuilder;
+    }
+
+    public RouteBuilder getFtpRouteBuilder() {
+        if(null == ftpRouteBuilder) {
+            ftpRouteBuilder = new FTPRouteBuilder();
+            ftpRouteBuilder.setFtpKnownHost(ftpKnownHost);
+            ftpRouteBuilder.setFtpPrivateKey(ftpPrivateKey);
+            ftpRouteBuilder.setFtpRemoteServer(ftpRemoteServer);
+            ftpRouteBuilder.setFtpUserName(ftpUserName);
+            ftpRouteBuilder.setReportsDirectory(reportsDirectory);
+        }
+        return ftpRouteBuilder;
     }
 }
---
+++ src/test/java/org/recap/route/FTPRouteBuilderTest.java
@@ -0,0 +1,19 @@
+package org.recap.route;
+
+import org.junit.Test;
+import org.recap.BaseTestCase;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by angelind on 21/7/16.
+ */
+public class FTPRouteBuilderTest extends BaseTestCase{
+
+    @Test
+    public void uploadFileToFTP() throws Exception {
+        System.out.println("Upload File to FTP");
+    }
+}
