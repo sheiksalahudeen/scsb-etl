5792974541d2b81fd76f39620a3bbf6bdc593333
angelindayana <angelin.dayana@gmail.com>
2016-07-27 16:07:33 +0530

    Added the logic to get content from the file uploaded in ftp.

--- src/test/java/org/recap/route/FTPRouteBuilderUT.java
+++ src/test/java/org/recap/route/FTPRouteBuilderUT.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.io.File;
+import java.io.FileWriter;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -37,21 +39,37 @@ public class FTPRouteBuilderUT extends BaseTestCase{
     @Test
     public void uploadFileToFTP() throws Exception {
         File file = new File(reportDirectoryPath + File.separator + "uploadTestFileToFTP.csv");
-        if(!file.exists()) {
-            file.createNewFile();
-        }
+        String content = "File \n Uploaded \n Successfully";
+        file.createNewFile();
         assertTrue(file.exists());
+        new FileWriter(file).append(content).flush();
         String fileName = reportDirectoryPath + File.separator + "uploadTestFileToFTP.csv";
         camelContext.addRoutes((new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("direct:start")
+                from("direct:uploadFile")
                         .process(new FTPUploadFileProcessor())
                         .to("sftp://" +ftpUserName + "@" + ftpRemoteServer + "?privateKeyFile="+ ftpPrivateKey + "&knownHostsFile=" + ftpKnownHost + "&fileName=${in.header.fileNameToUpload}");
             }
         }));
-        producer.sendBody("direct:start", fileName);
+        producer.sendBody("direct:uploadFile", fileName);
 
         Thread.sleep(1000);
+
+        camelContext.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:getFileContent")
+                        .pollEnrich("sftp://" +ftpUserName + "@" + ftpRemoteServer + "?privateKeyFile="+ ftpPrivateKey + "&knownHostsFile=" + ftpKnownHost + "&fileName=uploadTestFileToFTP.csv");
+            }
+        });
+
+        String response = producer.requestBody("direct:getFileContent", "", String.class);
+
+        Thread.sleep(1000);
+
+        assertNotNull(response);
+        System.out.println(response);
+        assertTrue(response.equals(content));
     }
 }
