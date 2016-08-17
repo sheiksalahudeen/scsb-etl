fb92164783c035934773f2e1ea301d022cf76f9a
peris <peri.subrahmanya@gmail.com>
2016-08-17 07:21:06 -0400

    ReportGenerator logic.

--- src/main/java/org/recap/repository/ReportDetailRepository.java
+++ src/main/java/org/recap/repository/ReportDetailRepository.java
@@ -2,6 +2,7 @@ package org.recap.repository;
 
 import org.recap.model.jpa.ReportDataEntity;
 import org.recap.model.jpa.ReportEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
@@ -11,4 +12,7 @@ import java.util.List;
  */
 public interface ReportDetailRepository extends PagingAndSortingRepository<ReportEntity, Integer> {
     List<ReportEntity> findByFileName(String fileName);
+
+    @Query(value = "",  nativeQuery = true)
+    Long findByFileAndDateRange();
 }
