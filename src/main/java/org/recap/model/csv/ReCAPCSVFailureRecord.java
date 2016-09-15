261d54d36d0e2e25392e5619273722fe288c0c52
angelindayana <angelin.dayana@gmail.com>
2016-09-15 21:34:05 +0530

    Added custom headers to the reports.

--- src/main/java/org/recap/model/csv/ReCAPCSVFailureRecord.java
+++ src/main/java/org/recap/model/csv/ReCAPCSVFailureRecord.java
@@ -14,29 +14,29 @@ import java.util.List;
 
 @CsvRecord(generateHeaderColumns = true, separator = ",", quoting = true, crlf = "UNIX", skipFirstLine = true)
 public class ReCAPCSVFailureRecord {
-    @DataField(pos = 1)
+    @DataField(pos = 1, columnName = "Owning Institution")
     private String owningInstitution;
-    @DataField(pos = 2)
+    @DataField(pos = 2, columnName = "Owning Institution Bib Id")
     private String owningInstitutionBibId;
-    @DataField(pos = 3)
+    @DataField(pos = 3, columnName = "Owning Institution Holdings Id")
     private String owningInstitutionHoldingsId;
-    @DataField(pos = 4)
+    @DataField(pos = 4, columnName = "Local Item Id")
     private String localItemId;
-    @DataField(pos = 5)
+    @DataField(pos = 5, columnName = "Item Barcode")
     private String itemBarcode;
-    @DataField(pos = 6)
+    @DataField(pos = 6, columnName = "Customer Code")
     private String customerCode;
-    @DataField(pos = 7)
+    @DataField(pos = 7, columnName = "Title")
     private String title;
-    @DataField(pos = 8)
+    @DataField(pos = 8, columnName = "Collection Group Designation")
     private String collectionGroupDesignation;
-    @DataField(pos = 9, pattern="MM/dd/yyyy hh:mm:ss a")
+    @DataField(pos = 9, pattern="MM/dd/yyyy hh:mm:ss a", columnName = "Create Date Item")
     private Date createDateItem;
-    @DataField(pos = 10, pattern="MM/dd/yyyy hh:mm:ss a")
+    @DataField(pos = 10, pattern="MM/dd/yyyy hh:mm:ss a", columnName = "Last Updated Date Item")
     private Date lastUpdatedDateItem;
-    @DataField(pos = 11)
+    @DataField(pos = 11, columnName = "Exception Message")
     private String exceptionMessage;
-    @DataField(pos = 12)
+    @DataField(pos = 12, columnName = "Error Description")
     private String errorDescription;
 
     @Ignore
