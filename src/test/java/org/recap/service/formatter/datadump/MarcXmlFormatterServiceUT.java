3049d957efa64364316e25d0e228f508af93dd8b
peris <peri.subrahmanya@gmail.com>
2016-11-11 07:16:10 -0500

    MarcXmlFormatterService test

--- src/test/java/org/recap/service/formatter/datadump/MarcXmlFormatterServiceUT.java
+++ src/test/java/org/recap/service/formatter/datadump/MarcXmlFormatterServiceUT.java
@@ -46,126 +46,98 @@ public class MarcXmlFormatterServiceUT extends BaseTestCase {
     @Value("${etl.dump.directory}")
     private String dumpDirectoryPath;
 
-    private String bibContent = "<collection>\n" +
-            "                <record>\n" +
-            "                    <controlfield tag=\"001\">NYPG002000036-B</controlfield>\n" +
-            "                    <controlfield tag=\"005\">20001116192424.2</controlfield>\n" +
-            "                    <controlfield tag=\"008\">850225r19731907nyu b 001 0 ara</controlfield>\n" +
-            "                    <datafield ind1=\" \" ind2=\" \" tag=\"010\">\n" +
-            "                        <subfield code=\"a\">   77173005  </subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\" \" ind2=\" \" tag=\"040\">\n" +
-            "                        <subfield code=\"c\">NN</subfield>\n" +
-            "                        <subfield code=\"d\">NN</subfield>\n" +
-            "                        <subfield code=\"d\">CStRLIN</subfield>\n" +
-            "                        <subfield code=\"d\">WaOLN</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\" \" ind2=\" \" tag=\"043\">\n" +
-            "                        <subfield code=\"a\">ff-----</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\"0\" ind2=\"0\" tag=\"050\">\n" +
-            "                        <subfield code=\"a\">DS36.6</subfield>\n" +
-            "                        <subfield code=\"b\">.I26 1973</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\"0\" ind2=\"0\" tag=\"082\">\n" +
-            "                        <subfield code=\"a\">910.031/767</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\"1\" ind2=\" \" tag=\"100\">\n" +
-            "                        <subfield code=\"a\">Ibn Jubayr, MuhÌ£ammad ibn AhÌ£mad,</subfield>\n" +
-            "                        <subfield code=\"d\">1145-1217.</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\"1\" ind2=\"0\" tag=\"245\">\n" +
-            "                        <subfield code=\"a\">RihÌ£lat</subfield>\n" +
-            "                        <subfield code=\"b\">AbÄ« al-Husayn Muhammad ibn Ahmad ibn Jubayr al-KinÄ\u0081nÄ« al-AndalusÄ«\n" +
-            "                            al-BalinsÄ«.\n" +
-            "                        </subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\" \" ind2=\" \" tag=\"250\">\n" +
-            "                        <subfield code=\"a\">2d ed.</subfield>\n" +
-            "                        <subfield code=\"b\">rev. by M. J. de Goeje and printed for the Trustees of the \"E. J. W. Gibb\n" +
-            "                            memorial\"\n" +
-            "                        </subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\" \" ind2=\" \" tag=\"260\">\n" +
-            "                        <subfield code=\"a\">[New York,</subfield>\n" +
-            "                        <subfield code=\"b\">AMS Press,</subfield>\n" +
-            "                        <subfield code=\"c\">1973] 1907.</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\" \" ind2=\" \" tag=\"300\">\n" +
-            "                        <subfield code=\"a\">363, 53 p.</subfield>\n" +
-            "                        <subfield code=\"c\">23 cm.</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\" \" ind2=\" \" tag=\"500\">\n" +
-            "                        <subfield code=\"a\">Added t.p.: The travels of Ibn Jubayr. Edited from a ms. in the University\n" +
-            "                            Library of Leyden by William Wright.\n" +
-            "                        </subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\" \" ind2=\" \" tag=\"500\">\n" +
-            "                        <subfield code=\"a\">Original ed. issued as v. 5 of \"E.J.W. Gibb memorial\" series.</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\" \" ind2=\" \" tag=\"504\">\n" +
-            "                        <subfield code=\"a\">Includes bibliographical references and index.</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\" \" ind2=\"0\" tag=\"651\">\n" +
-            "                        <subfield code=\"a\">Islamic Empire</subfield>\n" +
-            "                        <subfield code=\"x\">Description and travel.</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\"1\" ind2=\" \" tag=\"700\">\n" +
-            "                        <subfield code=\"a\">Wright, William,</subfield>\n" +
-            "                        <subfield code=\"d\">1830-1889.</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\"1\" ind2=\" \" tag=\"700\">\n" +
-            "                        <subfield code=\"a\">Goeje, M. J. de</subfield>\n" +
-            "                        <subfield code=\"q\">(Michael Jan),</subfield>\n" +
-            "                        <subfield code=\"d\">1836-1909.</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\"0\" ind2=\" \" tag=\"740\">\n" +
-            "                        <subfield code=\"a\">Travels of Ibn Jubayr.</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\" \" ind2=\"0\" tag=\"830\">\n" +
-            "                        <subfield code=\"a\">\"E.J.W. Gibb memorial\" series ;</subfield>\n" +
-            "                        <subfield code=\"v\">v.5.</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\" \" ind2=\" \" tag=\"907\">\n" +
-            "                        <subfield code=\"a\">.b100006279</subfield>\n" +
-            "                        <subfield code=\"c\">m</subfield>\n" +
-            "                        <subfield code=\"d\">a</subfield>\n" +
-            "                        <subfield code=\"e\">-</subfield>\n" +
-            "                        <subfield code=\"f\">ara</subfield>\n" +
-            "                        <subfield code=\"g\">nyu</subfield>\n" +
-            "                        <subfield code=\"h\">0</subfield>\n" +
-            "                        <subfield code=\"i\">3</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\" \" ind2=\" \" tag=\"952\">\n" +
-            "                        <subfield code=\"h\">*OAC (\"E. J. W. Gibb memorial\" series. v. 5)</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <datafield ind1=\" \" ind2=\" \" tag=\"952\">\n" +
-            "                        <subfield code=\"h\">*OFV 87-659</subfield>\n" +
-            "                    </datafield>\n" +
-            "                    <leader>01814cam a2200409 450000</leader>\n" +
-            "                </record>\n" +
-            "            </collection>";
+    private String bibContent = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+            "<collection>\n" +
+            "    <record>\n" +
+            "        <leader>00800cas a2200277 i 4500</leader>\n" +
+            "        <controlfield tag=\"001\">10</controlfield>\n" +
+            "        <controlfield tag=\"003\">NNC</controlfield>\n" +
+            "        <controlfield tag=\"005\">20100215174244.0</controlfield>\n" +
+            "        <controlfield tag=\"008\">810702c19649999ilufr p       0   a0engxd</controlfield>\n" +
+            "        <datafield ind1=\" \" ind2=\" \" tag=\"035\">\n" +
+            "            <subfield code=\"a\">(OCoLC)502399218</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\" \" ind2=\" \" tag=\"035\">\n" +
+            "            <subfield code=\"a\">(OCoLC)ocn502399218</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\" \" ind2=\" \" tag=\"035\">\n" +
+            "            <subfield code=\"a\">(CStRLIN)NYCG022-S</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\" \" ind2=\" \" tag=\"035\">\n" +
+            "            <subfield code=\"9\">AAA0010CU</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\" \" ind2=\" \" tag=\"035\">\n" +
+            "            <subfield code=\"a\">(NNC)10</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\" \" ind2=\" \" tag=\"040\">\n" +
+            "            <subfield code=\"a\">NNC</subfield>\n" +
+            "            <subfield code=\"c\">NNC</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\" \" ind2=\" \" tag=\"090\">\n" +
+            "            <subfield code=\"a\">TA434</subfield>\n" +
+            "            <subfield code=\"b\">.S15</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\"0\" ind2=\"0\" tag=\"245\">\n" +
+            "            <subfield code=\"a\">SOâ\u0082\u0083 abstracts &amp; newsletter.</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\"3\" ind2=\"3\" tag=\"246\">\n" +
+            "            <subfield code=\"a\">SO three abstracts &amp; newsletter</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\" \" ind2=\" \" tag=\"260\">\n" +
+            "            <subfield code=\"a\">[Chicago] :</subfield>\n" +
+            "            <subfield code=\"b\">United States Gypsum,</subfield>\n" +
+            "            <subfield code=\"c\">[1964?]-&lt;1979&gt;</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\" \" ind2=\" \" tag=\"300\">\n" +
+            "            <subfield code=\"a\">v. :</subfield>\n" +
+            "            <subfield code=\"b\">ill. ;</subfield>\n" +
+            "            <subfield code=\"c\">28 cm.</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\"0\" ind2=\" \" tag=\"362\">\n" +
+            "            <subfield code=\"a\">Vol. 1, no. 1-</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\" \" ind2=\" \" tag=\"500\">\n" +
+            "            <subfield code=\"a\">Editor: W.C. Hansen, 1964-1979.</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\" \" ind2=\"0\" tag=\"650\">\n" +
+            "            <subfield code=\"a\">Cement</subfield>\n" +
+            "            <subfield code=\"v\">Periodicals.</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\" \" ind2=\"0\" tag=\"650\">\n" +
+            "            <subfield code=\"a\">Gypsum</subfield>\n" +
+            "            <subfield code=\"v\">Periodicals.</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\"1\" ind2=\" \" tag=\"700\">\n" +
+            "            <subfield code=\"a\">Hansen, W. C.</subfield>\n" +
+            "            <subfield code=\"q\">(Waldemar Conrad),</subfield>\n" +
+            "            <subfield code=\"d\">1896-</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\"2\" ind2=\" \" tag=\"710\">\n" +
+            "            <subfield code=\"a\">United States Gypsum Co.</subfield>\n" +
+            "        </datafield>\n" +
+            "    </record>\n" +
+            "</collection>\n";
 
-    private String holdingContent = "<collection>\n" +
-            "                    <record>\n" +
-            "                        <datafield ind1='0' ind2='1' tag='852'>\n" +
-            "                            <subfield code='b'>rcppa</subfield>\n" +
-            "                            <subfield code='t'>1</subfield>\n" +
-            "                            <subfield code='h'>DF802</subfield>\n" +
-            "                            <subfield code='i'>.xP45</subfield>\n" +
-            "                            <subfield code='x'>tr fr anxafst</subfield>\n" +
-            "                        </datafield>\n" +
-            "                        <datafield ind1=' ' ind2='0' tag='866'>\n" +
-            "                            <subfield code='x'>DESIGNATOR: t.</subfield>\n" +
-            "                        </datafield>\n" +
-            "                    </record>\n" +
-            "                </collection>";
+    private String holdingContent = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+            "<collection>\n" +
+            "    <record>\n" +
+            "        <datafield ind1=\"0\" ind2=\"1\" tag=\"852\">\n" +
+            "            <subfield code=\"b\">off,che</subfield>\n" +
+            "            <subfield code=\"h\">QD79.C454 H533</subfield>\n" +
+            "        </datafield>\n" +
+            "        <datafield ind1=\"0\" ind2=\"0\" tag=\"866\">\n" +
+            "            <subfield code=\"a\">v.1-v.5</subfield>\n" +
+            "        </datafield>\n" +
+            "    </record>\n" +
+            "</collection>\n";
 
     @Test
     public void getMarcRecords() throws IOException, URISyntaxException {
         BibliographicEntity bibliographicEntity = getBibliographicEntity();
         Map<String, Object> successAndFailureFormattedList = (Map<String, Object>) marcXmlFormatterService.getFormattedOutput(Arrays.asList(bibliographicEntity));
-        List<Record> recordList = readMarcXml((String) successAndFailureFormattedList.get(ReCAPConstants.DATADUMP_FORMATTEDSTRING));
+        String marcXmlString = (String) successAndFailureFormattedList.get(ReCAPConstants.DATADUMP_FORMATTEDSTRING);
+        System.out.println(marcXmlString);
+        List<Record> recordList = readMarcXml(marcXmlString);
         assertNotNull(recordList);
         assertEquals("SCSB-100", recordList.get(0).getControlFields().get(0).getData());
     }
