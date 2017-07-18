0f9f3ee046d5a274fd665c8426399c3be6f0b47e
Prem Kumar <premkumar.balu@htcindia.com>
2017-07-18 17:56:58 +0530

    RECAP-859 - DataDump - Deleted feed changes

--- src/main/java/org/recap/service/formatter/datadump/DeletedJsonFormatterService.java
+++ src/main/java/org/recap/service/formatter/datadump/DeletedJsonFormatterService.java
@@ -111,7 +111,8 @@ public class DeletedJsonFormatterService implements DataDumpFormatterInterface {
 
     private boolean isChangedToPrivateCGD(ItemEntity fetchedItemEntity){
         if(fetchedItemEntity.getCgdChangeLog()!=null){
-            if(fetchedItemEntity.getCgdChangeLog().equals("SharedToPrivate") || fetchedItemEntity.getCgdChangeLog().equals("OpenToPrivate")){
+            if(fetchedItemEntity.getCgdChangeLog().equals(RecapConstants.CGD_CHANGE_LOG_SHARED_TO_PRIVATE) ||
+                    fetchedItemEntity.getCgdChangeLog().equals(RecapConstants.CGD_CHANGE_LOG_OPEN_TO_PRIVATE)){
                 return true;
             }
         }
